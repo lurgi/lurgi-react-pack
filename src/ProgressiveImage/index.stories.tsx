515199a9920666110c56036c3814407ca955b856
lurgi <wahoo9040@naver.com>
2024-06-28 16:07:52 +0900

    test: 용량에 따른 Progressive Loading 적용 스토리북 테스트 작성

--- src/ProgressiveImage/index.stories.tsx
+++ src/ProgressiveImage/index.stories.tsx
@@ -1,11 +1,18 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Image from ".";
 
+import LowerImage from "../assets/happy-dog.jpg";
+import HigherImage from "../assets/clock.jpeg";
+
 const meta = {
   title: "Components/Image",
   component: Image,
   argTypes: {
     src: { description: "이미지 경로를 설정합니다." },
+    style: { description: "이미지 요소에 해당하는 style css를 설정합니다." },
+  },
+  args: {
+    style: { maxHeight: "200px" },
   },
 } satisfies Meta<typeof Image>;
 
@@ -13,9 +20,16 @@ export default meta;
 
 type Stroy = StoryObj<typeof meta>;
 
-export const Default: Stroy = {
-  name: "Progressive Loading을 확인합니다.",
+export const Loading: Stroy = {
+  name: "Progressive Loading을 적용합니다.",
   args: {
-    src: "./",
+    src: HigherImage,
+  },
+};
+
+export const NoLoading: Stroy = {
+  name: "100KB보다 작은 이미지는 Progressive Loading을 적용하지 않습니다.",
+  args: {
+    src: LowerImage,
   },
 };
