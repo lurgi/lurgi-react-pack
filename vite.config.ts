c80f860d9ff5ada010538b377e1054a771efdd04
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-09-03 11:29:27 +0900

    chore: d.ts 생성 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "react-lurgi-pack",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
+    },
+  },
+  plugins: [react(), dts()],
+});
